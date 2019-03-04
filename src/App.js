b2975ed3ccacbb1989c126c88ec52a5269520407
Wade Ren <wr@3dsafety.com.au>
2019-03-04 16:57:42 +1100

    setup akita router bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
